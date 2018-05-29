9f1cab5618f0a22ba926f75c387c7b0cc675f964
Josue Mancilla <jmancilla@shutterfly.com>
2018-05-29 19:40:11 -0400

    probando pull request

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  
+  
+  static defaultProps={
+    cantidad:300  
+  };
+
+  static propTypes={
+    cantidad:PropTypes.number,
+    foo:PropTypes.string.isRequired,
+    telfs:PropTypes.arrayOf(PropTypes.number),
+    colores:PropTypes.oneOf(['verde','rojo']).isRequired
+  };
+
+
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          hola mundo  
+          <h2 {...this.props} >{this.props.foo}</h2>
+          {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App id="aaa" colores="verde" foo="bar" telfs={[223,3232,4434]} >
+    <h3>titulo h3</h3>
+    <h4>titulo h4</h4>
+</App>, document.getElementById('root'));
 registerServiceWorker();
