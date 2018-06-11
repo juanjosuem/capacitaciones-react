ad2cd30dd237cf38ab26ea6b3ef816d228c51774
antezananoemi <antezananoemi@yahoo.com>
2018-06-11 13:09:30 -0400

    sacar funcion del render

--- src/components/practica1/Navigation.js
+++ src/components/practica1/Navigation.js
@@ -4,49 +4,57 @@ import { Grid, Navbar,Nav,NavItem,NavDropdown,MenuItem, Button, FormGroup , Form
 
 
 class Navigation extends Component {
-
-	render() {
+	constructor(props) {
+		super(props);
+		this.renderDisplay = this.renderDisplay.bind(this);
 		const className = ' menu-active';
+		this.className = className;
 		const json = [
-		  { id: 33, type: 'link', text: 'Home', url: '#' },
-		  { id: 44, type: 'link', text: 'About Us', url: '#' },
-		  {
+			{ id: 33, type: 'link', text: 'Home', url: '#' },
+			{ id: 44, type: 'link', text: 'About Us', url: '#' },
+			{
 				id: 55,
 				type: 'dropdown',
-		    text: 'Functions',
-		    links: [
-		      { id: 6, type: 'link', text: 'Function 1', url: '#' },
-		      { id: 7, type: 'link', text: 'Function 2', url: '#' },
-		      { id: 8, type: 'link', text: 'Function 3', url: '#' },
-		    ]
-		  },
+				text: 'Functions',
+				links: [
+					{ id: 6, type: 'link', text: 'Function 1', url: '#' },
+					{ id: 7, type: 'link', text: 'Function 2', url: '#' },
+					{ id: 8, type: 'link', text: 'Function 3', url: '#' },
+				]
+			},
 		];
-		function DinamicNav(props) {
-			const content = props.json.map((item) =>
-			<NavDropdown key={item.id} title={item.text} id="basic-nav-dropdown">
-			{item.text === 'Functions' ? (
-					item.links.map((sub, index) =>
-						<MenuItem key={index}>
-						{sub.text}
-						</MenuItem>)
-			) :<MenuItem eventKey={item.id}>Separated link</MenuItem>}
+		this.json = json;
 
-			</NavDropdown>
+	}
 
-			);
+	renderDisplay() {
+		const content = this.props.json.map((item) =>
+		<NavDropdown key={item.id} title={item.text} id="basic-nav-dropdown">
+		{item.text === 'Functions' ? (
+				item.links.map((sub, index) =>
+					<MenuItem key={index}>
+					{sub.text}
+					</MenuItem>)
+		) :<MenuItem eventKey={item.id}>Separated link</MenuItem>}
 
-			return (
-				<Nav>
-					<NavItem href="#">
-						Link
-					</NavItem>
-					<NavItem href="#">
-						Link
-					</NavItem>
-				 {content}
-			 </Nav>
-			);
-		}
+		</NavDropdown>
+
+		);
+		return (
+
+			<Nav>
+				<NavItem href="#">
+					Link
+				</NavItem>
+				<NavItem href="#">
+					Link
+				</NavItem>
+			 {content}
+		 </Nav>
+		)
+	}
+
+	render() {
 		return (
 			<div>
 			<Navbar inverse fixedTop>
@@ -54,11 +62,11 @@ class Navigation extends Component {
 				<span >ICONO</span>
 					<Navbar.Header>
 						<Navbar.Brand>
-							<img src={logo} className={className} alt="logo" />
+							<img src={logo} className={this.className} alt="logo" />
 						</Navbar.Brand>
 						<Navbar.Toggle />
 					</Navbar.Header>
-				 <DinamicNav json={json} />
+				 this.renderDisplay()
 				 <Navbar.Collapse>
 					 <Navbar.Form pullLeft>
 						 <FormGroup>
@@ -75,3 +83,5 @@ class Navigation extends Component {
 }
 
 export default Navigation
+
+//<DinamicNav json={json} />
